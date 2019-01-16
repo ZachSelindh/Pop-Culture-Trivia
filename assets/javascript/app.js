a09598ffc02dcceb61340b6eaa5f62f4845d8b13
Zach Selindh <zachselindh@gmail.com>
2019-01-16 17:59:15 -0600

    Added a few trivia questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,13 +2,43 @@
 
 window.onload = function() {
 
-const timerTotal = 10;
+var questionArray = [
+    {
+        question: "What is the name of Phoebe's 'pet' rat from the show Friends?",
+        answers: [ "Bob", "Frank", "Tommy", "Eddie"],
+        correctAnswer: "Bob"
+    }, {
+        question: "In 'The X-Men,' what is Professor Xavier's first name?",
+        answers: [ "Charles", "Niles", "French", "Rupert"],
+        correctAnswer: "Charles"
+    }, {
+        question: "In 2008's 'Iron Man,' which actor portrayed character Obadiah Stane?",
+        answers: [ "Paul Bettany", "Clark Gregg", "Jeff Bridges", "Shaun Toub"],
+        correctAnswer: "Jeff Bridges"
+    }, {
+        question: "In his titular sitcom, what is Jerry Seinfeld's father's name?",
+        answers: [ "Abraham", "Mortimer", "Herb", "Benjamin"],
+        correctAnswer: "Mortimer"
+    },/*  {
+        question: "In his titular sitcom, what is Jerry Seinfeld's father's name?",
+        answers: [ "Abraham", "Mortimer", "Herb", "Benjamin"],
+        correctAnswer: "Mortimer"
+    } */
+
+
+]
+
+const timerInitial = 10;
+
+var playerWins = 0;
+
+var playerLosses = 0;
     
-var timeRemaining = timerTotal;
+var timeRemaining = timerInitial;
 
 var countingDown = false;
 
-$("#time-left").html(timerTotal);
+$("#time-left").html(timerInitial);
 
 $("#popup-text").html("Welcome to my pop-culture themed trivia game! If you don't know what Festivus is, what Mr. Narwhal told Buddy before he left the North Pole, or what guitar Wayne from Wayne's World was obsessed with, you might be in the wrong place. But, if you're ready to test your knowledge of otherwise-useless cultural phenomena, then press confirm to begin!");
 
@@ -17,9 +47,8 @@ $("#popup-card").fadeIn(500);
 function countDown() {
     if (countingDown == true) {
         timeRemaining--;
-        console.log(timeRemaining)
     } else {
-        timeRemaining = timerTotal;
+        timeRemaining = timerInitial;
     }
     $("#time-left").html(timeRemaining);
     outOfTime();
@@ -27,17 +56,21 @@ function countDown() {
 
 function outOfTime() {
     if (timeRemaining === 0) {
-        console.log("ZERO");
         countingDown = false;
         clearInterval(countDown);
         $("#popup-card, #continue-button").fadeIn(400);
         $("#popup-text").html("Out of time! No points for this round. Press continue to move on.");
+        playerLosses++;
     } 
+}
+
+function getQuestion() {
+    /* Get the question */
 }
 
 function gameElements() {
     setInterval(countDown, 1000);
-
+    getQuestion();
     outOfTime();
 }
 
