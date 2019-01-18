b11064564517b18586ff09c458d3a1720c9c67d0
Zach Selindh <zachselindh@gmail.com>
2019-01-17 20:13:08 -0600

    Added win/lose conditions and completed the game!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,6 +25,23 @@ var questionArray = [
         correctAnswer: "Mortimer"
     } */
 
+]
+
+var winReaction = [
+    {
+
+    }, {
+
+    }
+
+]
+
+var loseReaction = [
+    {
+
+    }, {
+        
+    }
 
 ]
 
@@ -44,11 +61,13 @@ $("#popup-text").html("Welcome to my pop-culture themed trivia game! If you don'
 
 $("#popup-card").fadeIn(500);
 
-function randomNum() {
-    x = [Math.floor(Math.random() * (questionArray.length - 0))]
+    /* Random number generator to pull a question randomly from the array. */
+function randomNum(x) {
+    x = [Math.floor(Math.random() * (x - 0))]
     return x;
 }
 
+    /* Resets the timer to the initial value if countingDown becomes false. */
 function countDown() {
     if (countingDown == true) {
         timeRemaining--;
@@ -59,6 +78,8 @@ function countDown() {
     outOfTime();
 }
 
+    /* Checks to see if the timer hits zero. If so, the clock stops and alerts the player they ran out of time.
+    After clicking continue will call the getQuestion function to move on. */
 function outOfTime() {
     if (timeRemaining === 0) {
         countingDown = false;
@@ -68,48 +89,85 @@ function outOfTime() {
     } 
 }
 
+    /* Selects the question using a random number, removes that question from the array, and adds the 
+    question and its relevant answers to the html elements. */
 function getQuestion() {
-    var indexOfQuestion = randomNum();
+    var indexOfQuestion = randomNum(questionArray.length);
     questionCurrent = questionArray[indexOfQuestion];
     questionArray.splice(indexOfQuestion, 1);
-    console.log(questionCurrent);
     $("#trivia-question").html(questionCurrent.question);
     for(i = 1; i <= 4; i++) {
         $("#answer" + i).html(questionCurrent.answers[i - 1]);
-      }
+      }  
 }
 
+
+    /* Creates the list items in the answer list with ids 1-4 */
 function getAnswers() {
     for(i = 1; i <= 4; i++) {
         $("#answer-list").append('<li id="answer'+ i +'" />');
     }
 }
 
+
+    /* Sets the win and lose conditions for the game. When the question array has run out, the game is over and the score is displayed. 
+    Otherwise, lets the player know if their answer was correct or not. */
 function winOrLose() {
     $("li").click(function() {
         var submittedAnswer = $(this).text();
         if (submittedAnswer == questionCurrent.correctAnswer) {
             countingDown = false;
-            console.log("WIN");
-        }
-    });
+            $("#popup-card, #continue-button").fadeIn(400);
+            $("#popup-text").html("Your answer was correct!")
+
+            /* Reaction gifs!!! */
+            playerWins ++;
+        } else {
+            countingDown = false;
+            $("#popup-card, #continue-button").fadeIn(400);
+            $("#popup-text").html("Bad luck! Your answer was super wrong.")
+
+            /* Reaction gifs!!! */
+        }       
+        }); 
 }
 
+    /* Function that starts the game by calling relevant functions and beginning the countdown clock. */
 function gameElements() {
     setInterval(countDown, 1000);
     getQuestion();
     getAnswers();
     outOfTime();
     winOrLose();
+} 
+
+    /* Sets the conditions for the game ending. Gives the user feedback on their score and 
+    offers them the chance to play again. */
+function gameOver() {
+    countingDown = false;
+    $("#continue-button").hide();
+    $("#popup-card, #reset-button").fadeIn(400);
+    $("#popup-text").html("Congratulations! The game has concluded. Your score was " + playerWins + ".");
+    $("#popup-text").append("<br>");
+    $("#popup-text").append("Press the Reset button to re-load the page and play again!");
 }
 
+    /* Sets up the functions of the buttons. */
 $("#confirm-button, #continue-button").click(function(){
+    if (questionArray.length == 0) {
+        gameOver();
+    } else {
     getQuestion();
     $("#popup-card").fadeOut(200);
     $("#confirm-button, #continue-button").hide();
     countingDown = true;
+    }
+});
+$("#reset-button").click(function(){
+    location.reload()
 });
 
+    /* Begins the game. */
 gameElements();
 
 }
